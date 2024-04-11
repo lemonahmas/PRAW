3320b6b1100e17e2e5252a5eceb53132be9480ff
Aaron Li <lzm_aaron@outlook.com>
2024-04-11 18:30:36 +0800

    update

--- reddit_genshin.py
+++ reddit_genshin.py
@@ -1,24 +1,21 @@
-import asyncpraw as praw
+import asyncpraw
 import asyncio
 import time,json,sys
-import threading
 import codecs
 
 
-async def download_post(subreddit_obj:praw.reddit.Subreddit, post_record:dict,lock:threading.Lock):
+async def download_post(subreddit_obj:asyncpraw.reddit.Subreddit, post_record:dict):
     random_post = await subreddit_obj.random()
     #print(random_post.id)
-    with lock:
-        if random_post.id not in post_record.keys():
-            post_record.update({random_post.id:random_post.title})
-            with open(f"./data/genshin_impact.txt", "a", encoding="utf-8") as f:
-                f.write(random_post.title+"||"+str(random_post.score)+"\n")
-                f.write(random_post.selftext+"\n")
-                f.close()
+    if random_post.id not in post_record.keys():
+        post_record.update({random_post.id:random_post.title})
+        with open(f"./data/genshin_impact.txt", "a", encoding="utf-8") as f:
+            f.write(random_post.title+"||"+str(random_post.score)+"\n")
+            f.write(random_post.selftext+"\n")
 
 
 async def main():
-    reddit = praw.Reddit(
+    reddit = asyncpraw.Reddit(
         client_id="bmGuDTdLdtHxQp3E50YezA",
         client_secret="wUjqaisuKEv9DcDM2OtNvbcKOye1NQ",
         #password="PASSWORD",
@@ -26,31 +23,27 @@ async def main():
         #username="USERNAME",
     )
     
-    post_record_lock=threading.Lock()
 
     # 获取 'Genshin_Impact' subreddit 的热门帖子
     subreddit = await reddit.subreddit('Genshin_Impact') 
-
-    tasks = []
     post_record = json.load(open("./data/post_record.json", "r", encoding="utf-8"))
-    for _ in range(20):
-        task = asyncio.create_task(download_post(subreddit, post_record, post_record_lock))
-        tasks.append(task)
-    for task in tasks:
-        await task
-    #print(post_record)
-    with open("./data/post_record.json", "w", encoding="utf-8") as f:
-        json.dump(post_record, f)
-        f.close()
-    print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())+str(post_record)+"\n")
 
+    while True:
+        try:
+            tasks = []
+            for _ in range(5):
+                task = asyncio.create_task(download_post(subreddit, post_record))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
+            #print(post_record)
+            with open("./data/post_record.json", "w", encoding="utf-8") as f:
+                json.dump(post_record, f)
+            print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())+str(post_record)+"\n")
+        except Exception as e:
+            print("Exception!! "+e.__str__()+" | let's sleep 10 minutes")
+            await asyncio.sleep(600)
 
 
 if __name__ == "__main__":
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-    while True:
-        try:
-            asyncio.run(main())
-        except Exception as e:
-            print("Exception!! "+e.__str__()+" | let's sleep 10 minutes")
-            time.sleep(600)
+    asyncio.run(main())
